2e30e970e6982ecdfed365945d27fb7c55011c0e
Edenilson Dias <ededias4@gmail.com>
2020-09-10 12:07:50 -0300

    refatoring products building's

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+	$to = "rockybd1995@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
@@ -33,5 +33,3 @@
 	$body .= "</body></html>";
 
     $send = mail($to, $subject, $body, $headers);
-
-?>
